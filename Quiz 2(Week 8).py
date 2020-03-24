90a497672188f9478898fedb36ae0257b08fd5b4
Sandy_undefined <08sandysk@gmail.com>
2020-03-24 22:49:40 +0530

    done

---
+++ Quiz 2(Week 8).py
@@ -0,0 +1,13 @@
+import random
+def play():
+    amt=0
+    for i in range(0,100):
+        r=random.uniform(0,1)
+        if(r<0.5):
+            amt=amt+1
+    return amt
+s=0
+for i in range(0,100):
+    s=s+play()/100
+print(s)
+
---
+++ Quiz 3(Week 8).py
@@ -0,0 +1,10 @@
+dict_age={}
+dict_age["sandeep"]=20
+dict_age["sandy"]=10
+dict_age["sand"]=40
+dict_age["sandip"]=30
+dict1=dict_age
+l=dict_age.values()
+l[0]=90
+print(l)
+
